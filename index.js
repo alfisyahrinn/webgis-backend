c437ca7a0231ecae86b0c8a27501573c6f1f6d7f
alfisyahrinn <sahrin201@gmail.com>
2025-01-07 21:41:59 +0700

    backend webgis

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+const cors = require('cors')
+
+app.use(cors())
+const pool = new Pool({
+    user: 'postgres',          
+    host: 'localhost',          
+    database: 'webgis',  
+    password: 'root',  
+    port: 5432,                 
+});
+
+app.get('/', (req, res)=>{
+    return res.json({
+        name: "holla"
+    })
+})
+
+app.get('/api/cagar', async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT gid, nm_objekcb, nm_kategor, deskripsi, district, provinsi, ST_AsGeoJSON(geom)::json AS geometry FROM cagar_aceh"
+        );
+        const geojson = {
+            type: "FeatureCollection",
+            features: result.rows.map(row => ({
+                type: "Feature",
+                properties: { 
+                    gid: row.gid, 
+                    name: row.nm_objekcb, 
+                    kategori: row.nm_kategor,
+                    deskripsi: row.deskripsi,
+                    district: row.district,
+                    provinsi: row.provinsi,
+                },
+                geometry: row.geometry
+            }))
+        };
+        res.json(geojson);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching data');
+    }
+});
+app.get('/api/buildings', async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT gid, name, pendidikan, ST_AsGeoJSON(geom)::json AS geometry FROM building"
+        );
+        const geojson = {
+            type: "FeatureCollection",
+            features: result.rows.map(row => ({
+                type: "Feature",
+                properties: { gid: row.gid, name: row.name, pendidikan: row.pendidikan },
+                geometry: row.geometry
+            }))
+        };
+        res.json(geojson);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching data');
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
