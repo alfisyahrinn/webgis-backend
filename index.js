e03a0a09050a5e0b37b98c395995284becf60b47
alfisyahrinn <sahrin201@gmail.com>
2025-01-08 17:38:51 +0700

    add api insert cagar

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const app = express();
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.json())
 const pool = new Pool({
-    user: 'postgres',          
-    host: 'localhost',          
-    database: 'webgis',  
-    password: 'root',  
-    port: 5432,                 
+    user: 'postgres',
+    host: 'localhost',
+    database: 'webgis',
+    password: 'root',
+    port: 5432,
 });
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     return res.json({
         name: "holla"
     })
@@ -27,9 +28,9 @@ app.get('/api/cagar', async (req, res) => {
             type: "FeatureCollection",
             features: result.rows.map(row => ({
                 type: "Feature",
-                properties: { 
-                    gid: row.gid, 
-                    name: row.nm_objekcb, 
+                properties: {
+                    gid: row.gid,
+                    name: row.nm_objekcb,
                     kategori: row.nm_kategor,
                     deskripsi: row.deskripsi,
                     district: row.district,
@@ -44,6 +45,51 @@ app.get('/api/cagar', async (req, res) => {
         res.status(500).send('Error fetching data');
     }
 });
+
+app.post('/api/cagar', async (req, res) => {
+    const {
+        nm_objekcb,
+        nm_kategor,
+        slug_kateg,
+        deskripsi,
+        district,
+        province,
+        objectid,
+        provinsi,
+        longitude,
+        latitude   
+    } = req.body;
+    console.log(req.body)
+
+    try {
+        const result = await pool.query(
+            `INSERT INTO cagar_aceh (
+                nm_objekcb, 
+                nm_kategor, 
+                slug_kateg, 
+                deskripsi, 
+                district, 
+                province, 
+                objectid, 
+                provinsi,
+                geom
+            ) 
+            VALUES ('${nm_objekcb}', '${nm_kategor}', '${slug_kateg}', '${deskripsi}', '${district}', '${province}', ${objectid}, '${provinsi}',
+                    ST_GeomFromText('POINT(${longitude} ${latitude})', 4326)) `,
+        );
+        console.log("result", result)
+        res.status(201).json({
+            message: 'Data successfully inserted',
+            data: req.body  
+        });
+    } catch (err) {
+        console.error('Error inserting data:', err);
+        res.status(500).json({ error: 'Failed to insert data' });
+    }
+});
+
+
+
 app.get('/api/buildings', async (req, res) => {
     try {
         const result = await pool.query(
